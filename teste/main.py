e3c6ede3cc55e26219dde5f0273628efbd2c4f7b
Thiago Lopes <thiagolopes033@hotmail.com>
2023-08-10 10:15:52 -0300

    test

--- teste/main.py
+++ teste/main.py
@@ -1,4 +1,14 @@
-# dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
-# same as
-#dist = [list(i) for i in graph]
-#???
+# Original graph with sublists
+graph = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Using nested map functions
+dist_map = list(map(lambda i: list(map(lambda j: j, i)), graph))
+
+# Using list comprehension
+dist_list_comp = [list(i) for i in graph]
+
+# Testing if the two resulting lists are the same
+print(dist_map == dist_list_comp)  # Should print True
+print(dist_map)
+print(dist_list_comp)
+
